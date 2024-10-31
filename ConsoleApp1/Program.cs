66671507db696572a793812c3c4eab4557f9baae
Giulio Sorrentino <numerone@fastwebnet.it>
2024-10-31 23:35:09 +0100

    oras è un reader testuale completo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ using MailKit.Search;
 using MimeKit;
 using Org.BouncyCastle.Utilities;
 
-System.Console.WriteLine("Email reader from Giulio Sorrentino versione 0.1, This program is under GNU GPL 3.0 or in your humble opinion any later version");
+System.Console.WriteLine("Email reader from Giulio Sorrentino versione 0.2, This program is under GNU GPL 3.0, or in your humble opinion, any later version");
 if (args.Length != 3) {
     Console.WriteLine("Passare come parametri l'indirizzo imap, la login e la password.");
     Environment.Exit(1);
@@ -40,37 +40,45 @@ using (ImapClient client = new ImapClient())
             Console.WriteLine(e.Message);
             Environment.Exit(1);
         }
-
-        for (i = 0; i < inbox.Count; i++)
+        do
         {
-            message = inbox.GetMessage(i, cancel.Token);
-            Console.WriteLine("Soggetto {0}: {1}",i, message.Subject);
-        }
+            for (i = 0; i < inbox.Count; i++)
+            {
+                message = inbox.GetMessage(i, cancel.Token);
+                Console.WriteLine("Soggetto {0}: {1}", i, message.Subject);
+            }
 
 
-        Console.Write("Inserire il numero del messaggio da leggere: ");
-        s = Console.ReadLine();
-      try
-        {
-            i = Convert.ToInt32(s);
-      }
-        catch (FormatException ex)
-        {
-            Console.WriteLine("Non è un numero. Il programma termina.");
-            Environment.Exit(1);
-        }
-        try
-        {
-            message = inbox.GetMessage(i, cancel.Token);
-        } catch (ArgumentOutOfRangeException ex)
-        {
-            Console.WriteLine("Indice non esistente. Il programma termina.");
-            Environment.Exit(1);
+            Console.Write("Inserire il numero del messaggio da leggere: (un numero negativo per uscire)");
+            s = Console.ReadLine();
+            try
+            {
+                i = Convert.ToInt32(s);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Non è un numero. Il programma termina.");
+                Environment.Exit(1);
+            }
+            catch (OverflowException ex1)
+            {
+                Console.WriteLine("Non è un intero. Il programma termina.");
+                Environment.Exit(1);
 
-        }
-        Console.WriteLine(message.TextBody);
-
-
+            }
+            try
+            {
+                message = inbox.GetMessage(i, cancel.Token);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Indice non esistente. Il programma termina.");
+                Environment.Exit(1);
+            }
+            Console.WriteLine(message.TextBody);
+            Console.Write("Premere un tasto per continuare....");
+            Console.ReadLine();
+        } while (i > 0);
         client.Disconnect(true, cancel.Token);
     }
 }
